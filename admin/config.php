48e86c6bbd6a9c3bb7cb78011ea6e8bf6e48d5ab
pnechushkin <pnechushkin@gmail.com>
2017-05-13 17:57:47 +0300

    instal

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shopoc/admin/');
+define('HTTP_CATALOG', 'http://shopoc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shopoc/admin/');
+define('HTTPS_CATALOG', 'http://shopoc/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OpenServer/domains/ShopOC/admin/');
+define('DIR_SYSTEM', 'D:/OpenServer/domains/ShopOC/system/');
+define('DIR_LANGUAGE', 'D:/OpenServer/domains/ShopOC/admin/language/');
+define('DIR_TEMPLATE', 'D:/OpenServer/domains/ShopOC/admin/view/template/');
+define('DIR_CONFIG', 'D:/OpenServer/domains/ShopOC/system/config/');
+define('DIR_IMAGE', 'D:/OpenServer/domains/ShopOC/image/');
+define('DIR_CACHE', 'D:/OpenServer/domains/ShopOC/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OpenServer/domains/ShopOC/system/storage/download/');
+define('DIR_LOGS', 'D:/OpenServer/domains/ShopOC/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OpenServer/domains/ShopOC/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OpenServer/domains/ShopOC/system/storage/upload/');
+define('DIR_CATALOG', 'D:/OpenServer/domains/ShopOC/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
